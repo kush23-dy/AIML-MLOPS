e087eac9268f3f1b80ff4aa2607ea3c5c080b622
kush23-dy <kus.tho.rt23@dypatil.edu>
2026-03-10 10:49:34 +0530

    Update train.py

--- Exp2/train.py
+++ Exp2/train.py
@@ -1,7 +1,3 @@
-pip install numpy
-pip install scikit-learn
-pip install pandas
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
